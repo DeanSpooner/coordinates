2bd3eff2e0a455377e6f7ca233c69afbc7e25244
Dean Spooner <dean.spooner@me.com>
2024-04-03 19:57:46 +0100

    Test for SFX on live page.

--- src/utils/sounds.ts
+++ src/utils/sounds.ts
@@ -2,10 +2,21 @@ import sounds from "./sounds.json";
 
 type SoundName = keyof typeof sounds;
 
+// Determine the base path dynamically based on the environment
+const basePath = process.env.NODE_ENV === "production" ? "./" : "src/";
+
 export const playSound = (soundName: SoundName) => {
+  const soundPath = `${basePath}${sounds[soundName]}`;
+
+  if (!soundPath) {
+    console.error(`Sound "${soundName}" not found in sounds.json`);
+    return;
+  }
+
   const sound = new Howl({
-    src: [sounds[soundName]],
+    src: [soundPath],
+    preload: true,
   });
 
-  return sound.play();
+  sound.play();
 };
