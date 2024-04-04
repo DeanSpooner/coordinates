ed9a175fdf8856f9e910259acd5193c76ac73d35
Dean Spooner <dean.spooner@me.com>
2024-04-04 21:41:41 +0100

    Paths fixed for music/SFX.

--- src/utils/music.ts
+++ src/utils/music.ts
@@ -2,9 +2,6 @@ import music from "./music.json";
 
 type TrackName = keyof typeof music;
 
-// Determine the base path dynamically based on the environment
-const basePath = process.env.NODE_ENV === "development" ? "public/" : "./";
-
 export const getMusicPath = (trackName: TrackName) => {
-  return `${basePath}${music[trackName]}`;
+  return music[trackName];
 };
--- src/utils/sounds.ts
+++ src/utils/sounds.ts
@@ -2,11 +2,8 @@ import sounds from "./sounds.json";
 
 type SoundName = keyof typeof sounds;
 
-// Determine the base path dynamically based on the environment
-const basePath = process.env.NODE_ENV === "development" ? "public/" : "./";
-
 export const playSound = (soundName: SoundName, volume?: number) => {
-  const soundPath = `${basePath}${sounds[soundName]}`;
+  const soundPath = sounds[soundName];
 
   const audio = new Audio(soundPath);
 
